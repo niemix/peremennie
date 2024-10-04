9549490726e46b7176276a497f28ec21f9ba2805
niemix <aloe.niemi888@gmail.com>
2024-10-04 22:35:27 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,6 @@
+the_number_of_completed_hw = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = 1.5/12
+print ('курс', course_name, 'всего задач', the_number_of_completed_hw, 'затрачено часов', number_of_hours_spent, 'среднее время выполнения', time_for_one_task)
+
